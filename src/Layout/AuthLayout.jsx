5ea81f48e91d3ff06e9b01012e2e78e6477f88f5
ibrahim yasin <111618470+ibrahim607@users.noreply.github.com>
2025-11-13 01:07:04 +0200

    solving issue

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom";
 
 function AuthLayout() {
-<<<<<<< HEAD
-  return <div>{/* login / signup pages layout */}</div>;
-=======
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
       <Outlet />
     </div>
   );
->>>>>>> feature/login-signup
 }
 
 export default AuthLayout;
