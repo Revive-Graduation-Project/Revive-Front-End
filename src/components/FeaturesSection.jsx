f723f540e1a9e869a0e14c1e81bd4c82d0b3541b
M7mednsr <m7mednsr999@gmail.com>
2025-11-16 20:31:29 +0200

    Home page become responsive

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -18,21 +18,23 @@ export default function FeaturesSection() {
   ];
 
   return (
-    <section className="features py-20">
-      <h2 className="text-center text-5xl font-bold mb-10">How It Works</h2>
-      <div className="container mx-auto flex flex-wrap justify-center gap-16">
+    <section className="py-20">
+      <h2 className="text-center text-3xl lg:text-5xl font-bold mb-10">
+        How It Works
+      </h2>
+      <div className="max-w-6xl mx-auto items-center gap-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {features.map((feature, i) => (
           <div
             key={i}
-            className="text-center w-[350px] transition-transform duration-300 hover:scale-105"
+            className="text-center transition-transform duration-300 hover:scale-105"
           >
             <img
               src={feature.img}
               alt={feature.title}
-              className="mx-auto mb-4 w-[120px] h-[120px] object-contain"
+              className="mx-auto mb-4 w-24 h-24 sm:w-30 sm:h-30 object-contain"
             />
-            <h3 className=" text-xl underline mb-2">{feature.title}</h3>
-            <p className="text-gray-600 text-sm leading-relaxed max-w-[300px] mx-auto">
+            <h3 className=" text-lg md:text-xl underline mb-2">{feature.title}</h3>
+            <p className="text-gray-600 text-sm md:text-base leading-relaxed max-w-xs mx-auto">
               {feature.text}
             </p>
           </div>
