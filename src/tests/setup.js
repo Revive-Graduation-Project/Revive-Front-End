a09bb2806b18c37cb189eff6d026c69518eba190
M7mednsr <m7mednsr999@gmail.com>
2026-06-18 14:25:59 +0300

    ....

--- src/tests/setup.js
+++
@@ -1,8 +0,0 @@
-import { expect, afterEach } from 'vitest';
-import * as matchers from '@testing-library/jest-dom/matchers';
-import { cleanup } from '@testing-library/react';
-
-expect.extend(matchers);
-
-// Cleanup DOM after each test (RTL auto-cleanup requires global afterEach)
-afterEach(cleanup);
